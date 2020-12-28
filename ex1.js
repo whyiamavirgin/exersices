f5a53928cd3a5a90810100a04c401d9245e6e83b
whyiamavirgin <timofejadamia4@gmail.com>
2020-12-28 23:26:46 +0600

    second

---
+++ ex1.js
@@ -0,0 +1,15 @@
+// TODO : Задачи под цифрой 1..n
+// console.log('31' + ' 18' + ' 79')
+// console.log('47' + '  52' + '  150')
+// console.log('50\n10') // ! \n - перенос на другую строку 
+// console.log('5\n10\n21')
+// console.log('1\n\n2') 
+
+// let pi = Math.PI
+// console.log(pi.toFixed(3)) // ! .toFixed(n) - округляет до n знаков после запятой
+
+// let eil = Math.E
+// console.log(eil.toFixed(1))
+
+let a = prompt('Введите число :', ' ')
+console.log('Вы ввели число : ' + a)
